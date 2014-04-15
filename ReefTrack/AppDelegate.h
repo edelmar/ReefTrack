620150af71814a9986422d70fe79f4201a311d50
Chris Culos <chris.culos@studio-symposium.com>
2014-04-15 17:12:56 -0400

    Initial Commit

---
+++ ReefTrack/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReefTrack
+//
+//  Created by Studio Symposium on 4/15/14.
+//  Copyright (c) 2014 Studio Symposium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReefTrack/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ReefTrack
+//
+//  Created by Studio Symposium on 4/15/14.
+//  Copyright (c) 2014 Studio Symposium. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
